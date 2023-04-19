31d34b2a03c5099429ad784b1da3ccab7dfa4fe7
Nic Colebank <niccolebank@gmail.com>
2023-04-19 15:34:58 -0700

    most of project complete

--- projects/01-gradebook/project.py
+++ projects/01-gradebook/project.py
@@ -45,10 +45,10 @@ def projects_total(grades):
     max_string =  ' - Max Points'
     fr_string = '_free_response'
     for project in projects:
-        numerator = np.nan_to_num(grades[project])
-        denominator = grades[project + max_string]
-        if project + fr_string in grades:
-            numerator += np.nan_to_num(grades[project + fr_string])
+        numerator = grades[project].fillna(0)
+        denominator = grades[project + max_string].copy()
+        if (project + fr_string) in grades:
+            numerator += grades[project + fr_string].fillna(0)
             denominator += grades[project + fr_string + max_string]
         project_grades += numerator/denominator
     project_grades = project_grades/len(projects)
@@ -107,37 +107,69 @@ def lateness_penalty(col):
 
 
 def process_labs(grades):
-    ...
-
-
+    out = pd.DataFrame(index=grades.index)
+    labs = get_assignment_names(grades)['lab']
+    for lab in labs:
+        out[lab] = ((grades[lab] / grades[lab + ' - Max Points']) * lateness_penalty(grades[lab + ' - Lateness (H:M:S)'])).fillna(0)
+    return out
 # ---------------------------------------------------------------------
 # QUESTION 6
 # ---------------------------------------------------------------------
 
+def calc_lab(row):
+    wo_lowest = row.drop(row.idxmin())
+    return wo_lowest.mean()
 
 def lab_total(processed):
-    ...
+    return processed.apply(calc_lab, axis=1)
 
 
 # ---------------------------------------------------------------------
 # QUESTION 7
 # ---------------------------------------------------------------------
 
+def calc_disc_or_check(grades, names, category):
+    sums = np.zeros(grades.shape[0])
+    for x in names[category]:
+        column_copy = grades[x].fillna(0)
+        sums += column_copy / grades[x + ' - Max Points']
+    return 0.025 * (sums / len(names[category]))
+
 
 def total_points(grades):
-    ...
+    names = get_assignment_names(grades)
+    lab_portion = 0.2 * lab_total(process_labs(grades))
+    project_portion = 0.3 * projects_total(grades)
+    checkpoints_portion = calc_disc_or_check(grades, names, 'checkpoint')
+    discussions_portion = calc_disc_or_check(grades, names, 'disc')
+    midterm_portion = 0.15 * (grades['Midterm'].fillna(0) / grades['Midterm - Max Points'])
+    final_portion = 0.3 * (grades['Final'].fillna(0) / grades['Final - Max Points'])
+    # print(lab_portion[0], project_portion[0], checkpoints_portion[0], discussions_portion[0], midterm_portion[0], final_portion[0])
+    return lab_portion + project_portion + checkpoints_portion + discussions_portion + midterm_portion + final_portion
 
 
 # ---------------------------------------------------------------------
 # QUESTION 8
 # ---------------------------------------------------------------------
 
+def letter_calc(grade):
+    if grade >= 0.9:
+        return 'A'
+    elif grade >= 0.8:
+        return 'B'
+    elif grade >= 0.7:
+        return 'C'
+    elif grade >= 0.6:
+        return 'D'
+    else:
+        return 'F'
 
 def final_grades(total):
-    ...
+    return total.apply(letter_calc)
 
 def letter_proportions(total):
-    ...
+    letters = final_grades(total)
+    return letters.value_counts().sort_values(ascending=False) / letters.size
 
 
 # ---------------------------------------------------------------------
@@ -146,10 +178,19 @@ def letter_proportions(total):
 
 
 def raw_redemption(final_breakdown, question_numbers):
-    ...
+    out = pd.DataFrame()
+    out['PID'] = final_breakdown['PID']
+    out['Raw Redemption Score'] = 0
+    denominator = 0
+    redemption_only = final_breakdown.iloc[:, question_numbers]
+    for col in redemption_only:
+        out['Raw Redemption Score'] += redemption_only[col].fillna(0)
+        denominator += redemption_only[col].max()
+    out['Raw Redemption Score'] = out['Raw Redemption Score'] / denominator
+    return out 
     
 def combine_grades(grades, raw_redemption_scores):
-    ...
+    return grades.merge(raw_redemption_scores)
 
 
 # ---------------------------------------------------------------------
@@ -158,7 +199,7 @@ def combine_grades(grades, raw_redemption_scores):
 
 
 def z_score(ser):
-    ...
+    return (ser - ser.mean()) / ser.std(ddof=0)
     
 def add_post_redemption(grades_combined):
     ...
