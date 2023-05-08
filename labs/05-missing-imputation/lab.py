a4fc7ba1f122013f9e6557f5fa557b62a60f38bc
Nic Colebank <niccolebank@gmail.com>
2023-05-08 12:17:21 -0700

    completed lab 5

--- labs/05-missing-imputation/lab.py
+++ labs/05-missing-imputation/lab.py
@@ -13,7 +13,7 @@ from scipy import stats
 
 
 def after_purchase():
-    ...
+    return ['NMAR', 'MD', 'MAR', 'NMAR', 'MAR']
 
 
 # ---------------------------------------------------------------------
@@ -22,7 +22,7 @@ def after_purchase():
 
 
 def multiple_choice():
-    ...
+    return ['MAR', 'MAR', 'MAR', 'NMAR', 'MCAR']
 
 
 # ---------------------------------------------------------------------
@@ -32,11 +32,11 @@ def multiple_choice():
 
 
 def first_round():
-    ...
+    return [0.1743, 'NR']
 
 
 def second_round():
-    ...
+    return [0.017, 'R', 'D']
 
 
 # ---------------------------------------------------------------------
@@ -45,7 +45,13 @@ def second_round():
 
 
 def verify_child(heights):
-    ...
+    just_x = heights.iloc[:, 2:]
+    data = []
+    for col in just_x:
+        data.append(stats.ks_2samp(heights.loc[heights[col].isna(), 'father'], heights.loc[~heights[col].isna(), 'father']).pvalue)
+    return pd.Series(data, index=just_x.columns)
+
+
 
 
 # ---------------------------------------------------------------------
@@ -54,7 +60,8 @@ def verify_child(heights):
 
 
 def cond_single_imputation(new_heights):
-    ...
+    categorical = new_heights.assign(father=pd.qcut(new_heights['father'], 4))
+    return categorical.groupby('father')['child'].transform(lambda ser: ser.fillna(ser.mean()))
 
 
 # ---------------------------------------------------------------------
@@ -63,11 +70,19 @@ def cond_single_imputation(new_heights):
 
 
 def quantitative_distribution(child, N):
-    ...
+    hist, bins = np.histogram(child[child.isna() == False], 10, density=True)
+    hist = hist / hist.sum()
+    vals = []
+    for _ in range(N):
+        bin_index = np.random.choice(10, p=hist)
+        vals.append(np.random.uniform(bins[bin_index], bins[bin_index + 1]))
+    return np.array(vals)
 
 
 def impute_height_quant(child):
-    ...
+    n_missing = child.isna().sum()
+    child.loc[child.isna()] = quantitative_distribution(child, n_missing)
+    return child
 
 
 # ---------------------------------------------------------------------
@@ -76,4 +91,4 @@ def impute_height_quant(child):
 
 
 def answers():
-    ...
+    return ([1, 2, 2, 1], ['netflix.com', 'reddit.com'])
