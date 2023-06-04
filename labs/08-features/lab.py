7a7618bbe17c783dc9b7e44d779eac1472a18d24
Nic Colebank <niccolebank@gmail.com>
2023-06-04 14:06:15 -0700

    finished project 4

--- labs/08-features/lab.py
+++ labs/08-features/lab.py
@@ -6,6 +6,7 @@ import numpy as np
 import plotly.express as px
 import statsmodels.api as sm
 import os
+import itertools
 from sklearn.preprocessing import Binarizer, QuantileTransformer, FunctionTransformer
 
 import warnings
@@ -18,17 +19,22 @@ warnings.filterwarnings('ignore')
 
 
 def best_transformation():
-    ...
+    return 4
 
 
 # ---------------------------------------------------------------------
 # QUESTION 2
 # ---------------------------------------------------------------------
 
-
+def determine_ordinal(val, order):
+    return order.index(val)
 
 def create_ordinal(df):
-    ...
+    out = pd.DataFrame(index = df.index)
+    out['ordinal_cut'] = df['cut'].apply(lambda x : determine_ordinal(x, ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal']))
+    out['ordinal_color'] = df['color'].apply(lambda x : determine_ordinal(x, ['J', 'I', 'H', 'G', 'F', 'E', 'D']))
+    out['ordinal_clarity'] = df['clarity'].apply(lambda x : determine_ordinal(x, ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF']))
+    return out
 
 
 # ---------------------------------------------------------------------
@@ -36,15 +42,29 @@ def create_ordinal(df):
 # ---------------------------------------------------------------------
 
 
-
+def create_one_hot_col(df, col):
+    out = pd.DataFrame(index=df.index)
+    vals = df[col].unique()
+    for val in vals:
+        out['one_hot_' + col + '_' + val] = (df[col] == val).astype(int)
+    return out
 
 def create_one_hot(df):
-    ...
+    out = create_one_hot_col(df, 'cut')
+    return pd.concat([out, create_one_hot_col(df, 'color'), create_one_hot_col(df, 'clarity')], axis=1)
 
 
+def create_proportions_col(df, col):
+    proportions = df[col].value_counts() / len(df)
+    return df[col].map(proportions)
 
 def create_proportions(df):
-    ...
+    out = pd.DataFrame(index=df.index)
+    out['proportion_cut'] = create_proportions_col(df, 'cut')
+    out['proportion_color'] = create_proportions_col(df, 'color')
+    out['proportion_clarity'] = create_proportions_col(df, 'clarity')
+    return out
+
 
 
 # ---------------------------------------------------------------------
@@ -53,7 +73,12 @@ def create_proportions(df):
 
 
 def create_quadratics(df):
-    ...
+    out = pd.DataFrame(index=df.index)
+    numeric_columns = df.select_dtypes(include=np.number).drop(columns=['price'])
+    combinations = itertools.combinations(numeric_columns.columns, 2)
+    for combination in combinations:
+        out[combination[0] + ' * ' + combination[1]] = df[combination[0]] * df[combination[1]]
+    return out
 
 
 # ---------------------------------------------------------------------
@@ -64,7 +89,7 @@ def create_quadratics(df):
 
 def comparing_performance():
     # create a model per variable => (variable, R^2, RMSE) table
-    ...
+    return [0.8493305264355833, 1548.5331930613177, 'x', 'carat * x', 'ordinal_color', 1434.8400089047332]
 
 
 # ---------------------------------------------------------------------
@@ -79,12 +104,19 @@ class TransformDiamonds(object):
         
     # Question 6.1
     def transform_carat(self, data):
-        ...
+        bi = Binarizer(threshold=1)
+        return bi.transform(data[['carat']])
     
     # Question 6.2
     def transform_to_quantile(self, data):
-        ...
+        quantile = QuantileTransformer(n_quantiles=100)
+        fitted = quantile.fit(self.data[['carat']])
+        return fitted.transform(data[['carat']])
+    
+    def calc_depth_pct(self, data):
+        return 100 * ((2 * data['z']) / (data['x'] + data['y']))
     
     # Question 6.3
     def transform_to_depth_pct(self, data):
-        ...
+        f = FunctionTransformer(self.calc_depth_pct)
+        return f.transform(data[['x', 'y', 'z']])
--- projects/04-language-models/project.py
+++ projects/04-language-models/project.py
@@ -123,31 +123,83 @@ class NGramLM(object):
             self.prev_mdl = NGramLM(N-1, tokens)
 
     def create_ngrams(self, tokens):
-        ...
+        n = self.N
+        i = 0
+        out = []
+        while i < len(tokens) - n + 1:
+            out.append(tuple(tokens[i] for i in range(i,i+n)))
+            i += 1
+        return out
+
         
     def train(self, ngrams):
         # N-Gram counts C(w_1, ..., w_n)
-        ...
+        ngram_series = pd.Series(ngrams)
+        ngram_counts = ngram_series.value_counts()
         
         # (N-1)-Gram counts C(w_1, ..., w_(n-1))
-        ...
-
+        n1gram_counts = ngram_series.apply(lambda x : x[:-1]).value_counts()
+        last_n1gram = ngrams[-1]
+        last_n1gram = last_n1gram[-self.N + 1:]
+        if last_n1gram in set(n1gram_counts.index):
+            n1gram_counts[last_n1gram] += 1
         # Create the conditional probabilities
-        ...
-        
-        # Put it all together
+        data = []
+        for ngram, count in ngram_counts.items():
+            n1gram = ngram[:-1]
+            n1gram_count = n1gram_counts[n1gram]
+            prob = count / n1gram_count
+            data.append((ngram, n1gram, prob))
 
-        ...
-        ...
+        return pd.DataFrame(data, columns=['ngram', 'n1gram', 'prob'])
     
+    def get_prob(self, mdl, ngram):
+        if isinstance(mdl, pd.Series): #is unigram model
+            if ngram[0] in set(mdl.index):
+                return mdl[ngram[0]]
+            else:
+                return 0
+        else:
+            if mdl['ngram'].isin([tuple(ngram)]).any():
+                return mdl[mdl['ngram'] == tuple(ngram)].iloc[0]['prob']
+            else:
+                return 0
+        
     def probability(self, words):
-        ...
-    
+        prob = 1
+        prev_mdl = self.prev_mdl
+        for i in range(self.N - 1, 0, -1):
+            if (i < len(words) + 1):
+                res = self.get_prob(prev_mdl.mdl, words[0:i])
+                prob *= res
+
+            if (i != 1):
+                prev_mdl = prev_mdl.prev_mdl
+        for i in range(len(words) - self.N + 1):
+            res = self.get_prob(self.mdl, words[i:i+self.N])
+            prob *= res
+        return prob
+                
+
+    def generate_tokens(self, m, tokens):
+        if m == 0:
+            return
+        if self.N - 1 > len(tokens):
+            self.prev_mdl.generate_tokens(m-1, tokens)
+        prev = tuple(tokens[-self.N + 1:])
+        if str(prev) in self.mdl['n1gram'].astype(str).values:
+            possibilities = self.mdl[self.mdl['n1gram'] == prev]
+            tokens.append(possibilities['ngram'].sample().iloc[0][-1])
+        else:
+            tokens.append('\x03')
+        self.generate_tokens(m-1, tokens)
+
 
     def sample(self, M):
         # Use a helper function to generate sample tokens of length `length`
-        ...
+        tokens = ['\x02']
+        self.generate_tokens(M-1, tokens)
+        tokens.append('\x03')
         
         # Transform the tokens to strings
-        ...
-        ...
+        return ' '.join(tokens)
